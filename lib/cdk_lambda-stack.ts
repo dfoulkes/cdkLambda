0f888ba82692988612b288d79cedc4cea09ed920
dfoulkes <danfoulkes@gmail.com>
2022-10-05 20:02:57 +0100

    lambda example with security group

--- lib/cdk_lambda-stack.ts
+++ lib/cdk_lambda-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as lb from "aws-cdk-lib/aws-lambda"
 
 export class CdkLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'vpc', {
+      cidr: "10.250.0.0/16",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "private-subnet",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,  //this will generate the NATs needed.
+        },
+        {
+          cidrMask: 24,
+          name: "public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+      maxAzs: 1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaSecurityGroup = new ec2.SecurityGroup(this, "lbSecurityGroup", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: 'Lambda Security Group'
+    });
+
+    //allow the lambda to talk to the internet only over HTTPS, we shouldn't trust port 80
+    lambdaSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+    lambdaSecurityGroup.addEgressRule(ec2.Peer.anyIpv6(), ec2.Port.tcp(443));
+
+
+    new lb.Function(this, 'ExampleLambda', {
+      code: lb.Code.fromAsset('./LambdaFunctions'),
+      functionName: 'GenerateRandomNumber',
+      handler: 'generateRandomNumber.lambdaHandler',
+      timeout: cdk.Duration.seconds(300),
+      runtime: lb.Runtime.NODEJS_16_X,
+      vpc:vpc,
+      vpcSubnets: {
+            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroups: [lambdaSecurityGroup],
+    });
+
   }
 }
